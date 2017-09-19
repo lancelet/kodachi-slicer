d3c506c1561a9f7fea39f4f6bb755ec50981bac1
Jonathan Merritt <j.s.merritt@gmail.com>
2017-09-19 19:52:22 +1000

    Initial GCode parser

--- src/Kodachi/GCode/Internal/Tokenizer.hs
+++ src/Kodachi/GCode/Internal/Tokenizer.hs
@@ -1,8 +1,43 @@
 module Kodachi.GCode.Internal.Tokenizer where
 
-import Kodachi.GCode.Internal.Types (Token(..))
+import           Kodachi.GCode.Internal.Types     (Token (..))
 
-import qualified Data.Attoparsec.ByteString.Char8 as A (scientific, Parser)
+import           Control.Applicative              (many, (<|>))
+import           Data.Attoparsec.ByteString       (Parser, choice, endOfInput,
+                                                   takeTill, takeWhile1)
+import qualified Data.Attoparsec.ByteString.Char8 as C8 (anyChar, char,
+                                                         endOfLine, isEndOfLine,
+                                                         isSpace_w8, scientific)
 
-tokParser :: A.Parser (Token m r)
-tokParser = undefined
+import           Prelude                          hiding (takeWhile)
+
+
+
+tokenListParser :: Parser [Token]
+tokenListParser = many tokenParser
+
+
+tokenParser :: Parser Token
+tokenParser = choice
+              [ whitespaceParser
+              , commentParser
+              , gcodeParser
+              ]
+
+  where
+
+    whitespaceParser :: Parser Token
+    whitespaceParser = Tok_WhiteSpace
+                   <$> takeWhile1 C8.isSpace_w8
+
+    commentParser :: Parser Token
+    commentParser = Tok_Comment
+                <$> (  C8.char ';'
+                    *> takeWhile1 (not . C8.isEndOfLine)
+                    <* (C8.endOfLine <|> endOfInput)
+                    )
+
+    gcodeParser :: Parser Token
+    gcodeParser = Tok_GCode
+              <$> C8.anyChar
+              <*> C8.scientific
--- src/Kodachi/GCode/Internal/Types.hs
+++ src/Kodachi/GCode/Internal/Types.hs
@@ -1,9 +1,10 @@
 module Kodachi.GCode.Internal.Types where
 
-import           Data.ByteString.Streaming.Char8 (ByteString)
-import           Data.Scientific                 (Scientific)
+import           Data.ByteString (ByteString)
+import           Data.Scientific (Scientific)
 
-data Token m r
-    = Tok_Whitespace (ByteString m r)
-    | Tok_Comment (ByteString m r)
+data Token
+    = Tok_WhiteSpace !ByteString
+    | Tok_Comment !ByteString
     | Tok_GCode !Char !Scientific
+    deriving (Eq, Show)
